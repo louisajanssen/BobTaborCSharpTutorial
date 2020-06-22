5379f4c2809e40e7b4edd9c00eddcc842bf83fee
louisajanssen <67242249+louisajanssen@users.noreply.github.com>
2020-06-21 17:00:52 -0700

    Build Timer

--- TimerExample/TimerExample/Program.cs
+++ TimerExample/TimerExample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace TimerExample
 {
@@ -10,6 +11,31 @@ namespace TimerExample
     {
         static void Main(string[] args)
         {
+            Timer myTimer = new Timer(2000);
+
+            myTimer.Elapsed += MyTimer_Elapsed;
+            myTimer.Elapsed += MyTimer_Elapsed1; ;
+
+            myTimer.Start();
+
+            Console.WriteLine("Press enter to remove the red event.");
+            Console.ReadLine();
+
+            myTimer.Elapsed -= MyTimer_Elapsed1;
+
+            Console.ReadLine();
+        }
+
+        private static void MyTimer_Elapsed1(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Elapsed1: {0:HH:mm:ss.fff}", e.SignalTime);
+        }
+
+        private static void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Elapsed: {0:HH:mm:ss.fff}", e.SignalTime);
         }
     }
 }
